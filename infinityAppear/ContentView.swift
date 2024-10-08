4f0c4f5e7689c556e0467658a794717273a99a94
enyuka <nakamura345@gmail.com>
2024-10-08 14:16:27 +0900

    First Commit

--- infinityAppear/ContentView.swift
+++ infinityAppear/ContentView.swift
@@ -8,14 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            List (array, id: \.self) { ary in
+                ButtonArea()
+            }
+            .refreshable {
+                print("refreshable")
+            }
         }
-        .padding()
+    }
+}
+
+
+struct ButtonArea: View {
+    @State private var flag = false
+    var body: some View {
+        Button(action: {
+            print("pushed")
+            flag = true
+        }) {
+            Image("icon_download").padding()
+        }.sheet(isPresented: $flag, onDismiss: {
+            print("dismiss")
+            flag = false
+        }) {
+            AudioPlayView()
+        }
+    }
+}
+
+struct AudioPlayView: View {
+    var body: some View {
+        ZStack {
+            Text("Hello, World!")
+        }
+        .onAppear {
+            print("onAppear")
+        }
+        .onDisappear {
+            print("onDisappear")
+        }
     }
 }
 
